50c0f3ee6910b36a77d8c8b4647ca4b52281fdb4
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2016-12-29 22:10:48 -0600

    Points now move rapidly

--- src/main/java/Voronoi.java
+++ src/main/java/Voronoi.java
@@ -2,6 +2,7 @@ import processing.core.PApplet;
 
 import java.util.ArrayList;
 
+
 /**
  * Created by dustin on 12/29/16.
  */
@@ -21,6 +22,7 @@ public class Voronoi extends PApplet {
     @Override
     public void draw() {
         clear();
+        movePoints();
         loadPixels();
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
@@ -32,6 +34,12 @@ public class Voronoi extends PApplet {
             ellipseMode(CENTER);
             fill(color(0));
             ellipse(point.x, point.y, 15, 15);
+        }
+    }
+
+    private void movePoints() {
+        for (Point point : points) {
+            point.move();
         }
     }
 
@@ -50,7 +58,17 @@ public class Voronoi extends PApplet {
 
     @Override
     public void mouseClicked() {
-        points.add(new Point(mouseX, mouseY, color(random(255), random(255), random(255))));
+        points.add(
+                new Point(
+                        mouseX,
+                        mouseY,
+                        color(
+                                random(255),
+                                random(255),
+                                random(255)
+                        ),
+                        random(0, 2 * PI)
+                ));
     }
 
     public static void main(String[] args) {
@@ -60,14 +78,21 @@ public class Voronoi extends PApplet {
 
 class Point {
     public int x, y, color;
+    float direction;
 
-    public Point(int x, int y, int color) {
+    public Point(int x, int y, int color, float direction) {
         this.x = x;
         this.y = y;
         this.color = color;
+        this.direction = direction;
     }
 
     public double distanceTo(int x, int y) {
         return Math.sqrt((this.x - x) * (this.x - x) + (this.y - y) * (this.y - y));
+    }
+
+    public void move() {
+        this.x = (int) (this.x + 2 * Math.cos(direction));
+        this.y = (int) (this.y + 2 * Math.sin(direction));
     }
 }
